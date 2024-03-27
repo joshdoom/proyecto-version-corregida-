d613600851b2cfc3228610b3aa7b01f19cb87bb6
joshdoom <156129144+joshdoom@users.noreply.github.com>
2024-03-27 12:16:41 -0400

    Add files via upload

--- src/utils/validate.py
+++ src/utils/validate.py
@@ -1,5 +1,11 @@
-def is_number(value):
-    if value.isdigit():
+def is_number(value, max_lenght: int):
+    if value.isdigit() and len(str(value)) <= max_lenght or value == '':
+        return True
+    else:
+        return False
+
+def is_letter(value):
+    if  value.isalpha() or value == '':
         return True
     else:
         return False
